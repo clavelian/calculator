b1a1a603896550ded517ea99c13208aff2230b99
Ian Clavel <ianclavel1991@gmail.com>
2024-07-24 21:58:54 +0800

    feat: added backbone for the app

--- calc/calc/app/page.tsx
+++ calc/calc/app/page.tsx
@@ -5,9 +5,27 @@ import { ModeToggle } from "@/components/lnd";
 export default function Home() {
   return (
     <>
-      <nav>
-        <ModeToggle />
+      <nav className="flex justify-end place-items-end">
+        <div className="pt-7 pr-7">
+          <ModeToggle />
+        </div>
       </nav>
+      <main className="flex justify-center place-items-center">
+        <div className="calculatorHolder p-5 border-foreground"></div>
+        <div className="screen p-10 ">
+          <div className="numpad">
+            <div>789</div>
+            <div>456</div>
+            <div>123</div>
+          </div>
+          <div className="operator">
+            <div>-</div>
+            <div>+</div>
+            <div>/</div>
+            <div>*</div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
