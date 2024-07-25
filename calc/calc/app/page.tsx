400fe9b355e87ae224c06d1b74438fe1a2114ccd
Ian Clavel <ianclavel1991@gmail.com>
2024-07-25 22:57:54 +0800

    added more structure for the app / minimized the progress due to blackout

--- calc/calc/app/page.tsx
+++ calc/calc/app/page.tsx
@@ -14,15 +14,33 @@ export default function Home() {
         <div className="calculatorHolder p-5 border-foreground"></div>
         <div className="screen p-10 ">
           <div className="numpad">
-            <div>789</div>
-            <div>456</div>
-            <div>123</div>
+            <div>%</div>
+            <div>CE</div>
+            <div>C</div>
+            <div>Del</div>
+            {/* 789 */}
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+            {/* 456 */}
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            {/* 123 */}
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            {/* -0. */}
+            <div>-</div>
+            <div>0</div>
+            <div>.</div>
           </div>
           <div className="operator">
+            <div>/</div>
+            <div>x</div>
             <div>-</div>
             <div>+</div>
-            <div>/</div>
-            <div>*</div>
+            <div>=</div>
           </div>
         </div>
       </main>
