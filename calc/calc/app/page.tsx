62399ab2cec6456e9a228210739eff92540c9da9
Ian Clavel <ianclavel1991@gmail.com>
2024-07-28 18:50:41 +0800

    feat: futher improving of the structure

--- calc/calc/app/page.tsx
+++ calc/calc/app/page.tsx
@@ -10,48 +10,49 @@ export default function Home() {
           <ModeToggle />
         </div>
       </nav>
+      {/* main */}
+      <div className="flex justify-center items-center py-10 ">
+        <div className="screen p-10 border-white rounded-sm border flex justify-center place-items-center"></div>
+      </div>
       <main className="flex justify-center place-items-center">
-        <div className="calculatorHolder p-5 border-foreground"></div>
-        <div className="screen p-10 ">
-          <div className="numpad">
-            <div className="etc">
-              <Button>%</Button>
-              <Button>CE</Button>
-              <Button>C</Button>
-              <Button>Del</Button>
-            </div>
-            {/* 789 */}
-            <div className="top">
-              <Button>7</Button>
-              <Button>8</Button>
-              <Button>9</Button>
-            </div>
-            {/* 456 */}
-            <div className="mid">
-              <Button>4</Button>
-              <Button>5</Button>
-              <Button>6</Button>
-            </div>
-            {/* 123 */}
-            <div className="low">
-              <Button>1</Button>
-              <Button>2</Button>
-              <Button>3</Button>
-            </div>
-            {/* -0. */}
-            <div className="lower">
-              <Button>-</Button>
-              <Button>0</Button>
-              <Button>.</Button>
-            </div>
+        <div className="calculatorHolder p-5 border border-white "></div>
+        <div className="numpad">
+          <div className="etc">
+            <Button className="mr-0.5">%</Button>
+            <Button className="mr-0.5">C</Button>
+            <Button className="mr-0.5">Del</Button>
           </div>
-          <div className="operator">
-            <Button>/</Button>
-            <Button>x</Button>
-            <Button>-</Button>
-            <Button>+</Button>
-            <Button>=</Button>
+          {/* 789 */}
+          <div className="top">
+            <Button className="text-xl mr-0.5 mt-0.5">7</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">8</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">9</Button>
           </div>
+          {/* 456 */}
+          <div className="mid">
+            <Button className="text-xl mr-0.5 mt-0.5">4</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">5</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">6</Button>
+          </div>
+          {/* 123 */}
+          <div className="low">
+            <Button className="text-xl mr-0.5 mt-0.5">1</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">2</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">3</Button>
+          </div>
+          {/* -0. */}
+          <div className="lower">
+            <Button className="text-xl mr-0.5 mt-0.5">-</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">0</Button>
+            <Button className="text-xl mr-0.5 mt-0.5">.</Button>
+          </div>
+        </div>
+        <div className="operator flex-col-reverse">
+          <Button className="mr-0.5 mt-0.5">/</Button>
+          <Button className="mr-0.5 mt-0.5">x</Button>
+          <Button className="mr-0.5 mt-0.5">-</Button>
+          <Button className="mr-0.5 mt-0.5">+</Button>
+          <Button className="mr-0.5 mt-0.5">=</Button>
         </div>
       </main>
     </>
