3ccc9867ea043c6cb2e1cabbca4b4995af3dea94
Ian Clavel <ianclavel1991@gmail.com>
2024-08-03 23:28:34 +0800

    minor improvement

--- calc/calc/app/page.tsx
+++ calc/calc/app/page.tsx
@@ -12,44 +12,47 @@ export default function Home() {
       </nav>
       {/* main */}
       <div className="flex justify-center items-center py-10 ">
-        <div className="screen p-10 border-white rounded-sm border flex justify-center place-items-center"></div>
+        <div className="screen p-10 border-white rounded-sm border flex justify-center place-items-center w-80">
+          <form action="input">zxc</form>
+        </div>
       </div>
       <main className="flex justify-center place-items-center">
-        <div className="calculatorHolder p-5 border border-white "></div>
-        <div className="numpad">
-          <div className="etc">
-            <Button className="button text-base">%</Button>
-            <Button className="button text-base">C</Button>
-            <Button className="button text-base">Del</Button>
-            <Button className="button text-base">/</Button>
-          </div>
-          {/* 789 */}
-          <div className="buttonTop">
-            <Button className="button text-xl ">7</Button>
-            <Button className="button text-xl ">8</Button>
-            <Button className="button text-xl ">9</Button>
-            <Button className="button text-xl">x</Button>
-          </div>
-          {/* 456 */}
-          <div className="buttonMid">
-            <Button className="button text-xl ">4</Button>
-            <Button className="button text-xl ">5</Button>
-            <Button className="button text-xl ">6</Button>
-            <Button className="button text-xl">-</Button>
-          </div>
-          {/* 123 */}
-          <div className="buttonLow">
-            <Button className="button text-xl ">1</Button>
-            <Button className="button text-xl ">2</Button>
-            <Button className="button text-xl ">3</Button>
-            <Button className="button text-xl">+</Button>
-          </div>
-          {/* -0. */}
-          <div className="buttonLower">
-            <Button className="button text-xl ">-</Button>
-            <Button className="button text-xl ">0</Button>
-            <Button className="button text-xl ">.</Button>
-            <Button className="button text-xl">=</Button>
+        <div className="calculatorHolder p-5 border border-white ">
+          <div className="numpad">
+            <div className="etc">
+              <Button className="button text-base">%</Button>
+              <Button className="button text-base">C</Button>
+              <Button className="button text-base">Del</Button>
+              <Button className="button text-base">/</Button>
+            </div>
+            {/* 789 */}
+            <div className="buttonTop">
+              <Button className="button text-xl ">7</Button>
+              <Button className="button text-xl ">8</Button>
+              <Button className="button text-xl ">9</Button>
+              <Button className="button text-xl">x</Button>
+            </div>
+            {/* 456 */}
+            <div className="buttonMid">
+              <Button className="button text-xl ">4</Button>
+              <Button className="button text-xl ">5</Button>
+              <Button className="button text-xl ">6</Button>
+              <Button className="button text-xl">-</Button>
+            </div>
+            {/* 123 */}
+            <div className="buttonLow">
+              <Button className="button text-xl ">1</Button>
+              <Button className="button text-xl ">2</Button>
+              <Button className="button text-xl ">3</Button>
+              <Button className="button text-xl">+</Button>
+            </div>
+            {/* -0. */}
+            <div className="buttonLower">
+              <Button className="button text-xl ">-</Button>
+              <Button className="button text-xl ">0</Button>
+              <Button className="button text-xl ">.</Button>
+              <Button className="button text-xl">=</Button>
+            </div>
           </div>
         </div>
       </main>
