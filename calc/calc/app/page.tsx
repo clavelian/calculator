a7304f6c7ff293545c79125415aa61867c87fca4
Ian Clavel <ianclavel1991@gmail.com>
2024-08-05 23:07:41 +0800

    major changes

--- calc/calc/app/page.tsx
+++ calc/calc/app/page.tsx
@@ -1,61 +1,18 @@
 import Image from "next/image";
-import { Button } from "../components/ui/button";
 import { ModeToggle } from "@/components/lnd";
 import { ShootingStarsAndStarsBackgroundDemo } from "../components/stars";
-
+import Calc from "../components/main";
 export default function Home() {
   return (
     <>
-      <ShootingStarsAndStarsBackgroundDemo />
-      <nav className="flex justify-end place-items-end">
-        <div className="pt-7 pr-7">
+      {/* crop the screen to a single page */}
+      <main className="">
+        <ShootingStarsAndStarsBackgroundDemo />
+        <nav className="relative top-[-97vh] left-10 h-0 w-0">
           <ModeToggle />
-        </div>
-      </nav>
-      {/* main */}
-      <div className="flex justify-center items-center py-10 ">
-        <div className="screen p-10 border-white rounded-sm border flex justify-center place-items-center w-80">
-          <form action="input">zxc</form>
-        </div>
-      </div>
-      <main className="flex justify-center place-items-center">
-        <div className="calculatorHolder p-5 border border-white ">
-          <div className="numpad">
-            <div className="etc">
-              <Button className="button text-base">%</Button>
-              <Button className="button text-base">C</Button>
-              <Button className="button text-base">Del</Button>
-              <Button className="button text-base">/</Button>
-            </div>
-            {/* 789 */}
-            <div className="buttonTop">
-              <Button className="button text-xl ">7</Button>
-              <Button className="button text-xl ">8</Button>
-              <Button className="button text-xl ">9</Button>
-              <Button className="button text-xl">x</Button>
-            </div>
-            {/* 456 */}
-            <div className="buttonMid">
-              <Button className="button text-xl ">4</Button>
-              <Button className="button text-xl ">5</Button>
-              <Button className="button text-xl ">6</Button>
-              <Button className="button text-xl">-</Button>
-            </div>
-            {/* 123 */}
-            <div className="buttonLow">
-              <Button className="button text-xl ">1</Button>
-              <Button className="button text-xl ">2</Button>
-              <Button className="button text-xl ">3</Button>
-              <Button className="button text-xl">+</Button>
-            </div>
-            {/* -0. */}
-            <div className="buttonLower">
-              <Button className="button text-xl ">-</Button>
-              <Button className="button text-xl ">0</Button>
-              <Button className="button text-xl ">.</Button>
-              <Button className="button text-xl">=</Button>
-            </div>
-          </div>
+        </nav>
+        <div className="relative top-[-89vh] h-0">
+          <Calc />
         </div>
       </main>
     </>
---
+++ calc/calc/components/main.tsx
@@ -0,0 +1,76 @@
+"use client";
+import react from "react";
+import { Button } from "../components/ui/button";
+import { useState } from "react";
+const Calc = () => {
+  // Problem: the number value is still under construction, provided with sample function
+
+  const [count, setCount] = useState(0);
+  const handleAdd = () => setCount(count + 1);
+  const handleSub = () => setCount(count - 1);
+  const handleMulti = () => setCount(count * 2);
+  const handleDiv = () => setCount(count / 2);
+  const handleReset = () => setCount(0);
+  // Del func is missing
+  // % func is missing
+  // - func is missing
+
+  return (
+    <>
+      <div className="flex justify-center items-center py-10 ">
+        <div className="screen p-10 border-white rounded-sm border flex justify-center place-items-center w-80">
+          <form action="input">{count}</form>
+          <b>{count}</b>
+        </div>
+      </div>
+      <main className="flex justify-center place-items-center">
+        <div className="calculatorHolder p-5 border border-white ">
+          <div className="numpad">
+            <div className="etc">
+              <Button className="button text-base">%</Button>
+              <Button onClick={handleReset} className="button text-base">
+                C
+              </Button>
+              <Button className="button text-base">Del</Button>
+              <Button className="button text-base">/</Button>
+            </div>
+            {/* 789 */}
+            <div className="buttonTop">
+              <Button className="button text-xl ">7</Button>
+              <Button className="button text-xl ">8</Button>
+              <Button className="button text-xl ">9</Button>
+              <Button className="button text-xl">x</Button>
+            </div>
+            {/* 456 */}
+            <div className="buttonMid">
+              <Button className="button text-xl ">4</Button>
+              <Button className="button text-xl ">5</Button>
+              <Button className="button text-xl ">6</Button>
+              <Button onClick={handleSub} className="button text-xl ">
+                -
+              </Button>
+            </div>
+            {/* 123 */}
+            <div className="buttonLow">
+              <Button className="button text-xl ">1</Button>
+              <Button className="button text-xl ">2</Button>
+              <Button className="button text-xl ">3</Button>
+              <Button onClick={handleAdd} className="button text-xl">
+                +
+              </Button>
+            </div>
+            {/* -0. */}
+            <div className="buttonLower">
+              <Button className="button text-xl">-</Button>
+              <Button className="button text-xl ">0</Button>
+              <Button className="button text-xl ">.</Button>
+              <Button className="button text-xl">=</Button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Calc;
